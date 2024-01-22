68751612ad0baaf73f205f28bb46a8736b5cd4ae
myhexolink <pliushchovo@gmail.com>
2024-01-23 00:25:37 +0200

    gdfgdfgdfgdf

--- messenger/src/main/java/com/messenger/util/ValidationUtil.java
+++ messenger/src/main/java/com/messenger/util/ValidationUtil.java
@@ -17,7 +17,6 @@ public class ValidationUtil {
     private static final Validator validator;
 
     static {
-        //  From Javadoc: implementations are thread-safe and instances are typically cached and reused.
         ValidatorFactory factory = Validation.buildDefaultValidatorFactory();
         //  From Javadoc: implementations of this interface must be thread-safe
         validator = factory.getValidator();
