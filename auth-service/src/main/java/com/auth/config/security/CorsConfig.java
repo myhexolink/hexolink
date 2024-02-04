725a8c7d4b4700957115db50debb08ffe2fcf36b
myhexolink <pliushchovo@gmail.com>
2024-02-04 14:48:28 +0200

    gdfgdfgdfgdf

--- auth-service/src/main/java/com/auth/config/security/CorsConfig.java
+++ auth-service/src/main/java/com/auth/config/security/CorsConfig.java
@@ -11,7 +11,7 @@ public class CorsConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOrigins("https://www.hexolink.xyz")
-                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowCredentials(true)
                 .allowedHeaders("*")
                 .maxAge(3600);
